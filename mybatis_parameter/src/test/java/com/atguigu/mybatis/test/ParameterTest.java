854bad3e6a2ddc522dac5c8b1a2431bc139fc7b0
horace <horace@163.com>
2022-08-07 20:50:26 +0800

    202208072050

---
+++ mybatis_parameter/src/test/java/com/atguigu/mybatis/test/ParameterTest.java
@@ -0,0 +1,65 @@
+package com.atguigu.mybatis.test;
+
+import com.atguigu.mybatis.mapper.UserMapper;
+import com.atguigu.mybatis.pojo.User;
+import com.atguigu.mybatis.utils.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+/**
+ * @Author Horace
+ * @Date 2022/8/7 16:09
+ * @Version 1.0
+ */
+public class ParameterTest {
+
+    @Test
+    public void testGetUserByUsername(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.getUserByUsername("admin");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testCheckLogin(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.checkLogin("admin","123456");
+        System.out.println(user);
+    }
+
+
+    @Test
+    public void testCheckLoginByMap(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("username","admin");
+        map.put("password","123456");
+        User user = mapper.checkLoginByMap(map);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsertUser(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.checkLoginByParam("admin","123456");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testCheckLoginByParam(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User(null,"root","123456",33,"女","123@qq.com");
+        mapper.insertUser(user);
+        System.out.println(user);
+    }
+
+
+
+}
